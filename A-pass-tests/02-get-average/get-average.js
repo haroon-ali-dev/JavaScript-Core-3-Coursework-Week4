d0600abe9319be8f71262afd3a5c08a3b0b36a92
Haroon Ali <87202358+haroon-ali-dev@users.noreply.github.com>
2022-11-20 14:04:04 +0000

    complete A

--- A-pass-tests/02-get-average/get-average.js
+++ A-pass-tests/02-get-average/get-average.js
@@ -2,6 +2,10 @@
 // return the average of all the numbers
 // be sure to exclude the strings
 
-function average(numbers) {}
+function average(numbers) {
+  onlyNumbers = numbers.filter(num => typeof num === "number");
+  const total = onlyNumbers.reduce((a, c) => a + c);
+  return total / onlyNumbers.length;
+}
 
 module.exports = average;
