2b61c1d948b120670692b2e6ea33be980a4288e0
Haroon Ali <87202358+haroon-ali-dev@users.noreply.github.com>
2022-11-20 18:53:01 +0000

    complete C-TDD 01

--- C-TDD/01-roman-numerals/convert-to-old-roman.js
+++ C-TDD/01-roman-numerals/convert-to-old-roman.js
@@ -1,5 +1,61 @@
-function convertToOldRoman(n) {
+// FOR TEST - "returns I if passed 1 as an argument"
+// function convertToOldRoman(n) {
+//   let roman = "";
 
+//   for (let i = 1; i <= n; i++) {
+//     roman += "I";
+//   }
+
+//   return roman;
+// }
+
+// console.log(convertToOldRoman(4));
+
+// FOR TEST - "returns VII if passed 7 as an argument"
+// function convertToOldRoman(n) {
+//   let roman = "";
+//   let howMany5s10s;
+
+//   howMany5s10s = Math.floor(n / 5);
+
+//   for (let i = 1; i <= howMany5s10s; i++) {
+//     roman += "V";
+//     n -= 5;
+//   }
+
+//   for (let i = 1; i <= n; i++) {
+//     roman += "I";
+//   }
+
+//   return roman;
+// }
+
+// FOR TEST - "returns XV if passed 15 as an argument"
+function convertToOldRoman(n) {
+  let roman = "";
+  let howMany5s10s;
+
+  howMany5s10s = Math.floor(n / 10);
+
+  for (let i = 1; i <= howMany5s10s; i++) {
+    roman += "X";
+    n -= 10;
+  }
+
+  howMany5s10s = Math.floor(n / 5);
+
+  for (let i = 1; i <= howMany5s10s; i++) {
+    roman += "V";
+    n -= 5;
+  }
+
+  for (let i = 1; i <= n; i++) {
+    roman += "I";
+  }
+
+  return roman;
 }
+
+console.log(convertToOldRoman(239));
 
 module.exports = convertToOldRoman;
--- C-TDD/01-roman-numerals/convert-to-old-roman.test.js
+++ C-TDD/01-roman-numerals/convert-to-old-roman.test.js
@@ -1,7 +1,15 @@
 let convertToOldRoman = require("./convert-to-old-roman");
 
-test("returns I if passed 1 as an argument", function () {
-  // Arrange
-  // Act
-  // Assert
+describe("convert to old roman", () => {
+  it("returns I if passed 1 as an argument", () => {
+    expect(convertToOldRoman(1)).toBe("I");
+  });
+
+  it("returns VII if passed 7 as an argument", () => {
+    expect(convertToOldRoman(7)).toBe("VII");
+  });
+
+  it("returns XV if passed 15 as an argument", () => {
+    expect(convertToOldRoman(15)).toBe("XV");
+  });
 });
